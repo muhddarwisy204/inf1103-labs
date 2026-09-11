73464c25a314e83636404529170bb8b325f1e8a2
Darwisy <2601678@sit.singaporetech.edu.sg>
2026-09-11 15:38:35 +0800

    Added stock quantity from user which converts to int

--- auditor.py
+++ auditor.py
@@ -1,7 +1,10 @@
 total_inventory = 0
 
+
 while True:
     user_input = input("Enter stock quantity (or 'quit' to exit): ").strip()
-
+    
     if user_input.lower() == "quit":
         break
+
+    stock_quantity = int(user_input)
